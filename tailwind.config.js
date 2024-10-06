c519e25e3ce90622cb24dd856ee22968573ec82d
Niraj-Paswan <paswanniraj324@@gmail.com>
2024-10-06 20:45:23 +0530

    Doned

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html"],
+  content: ["./dist/**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0D00A6",
+        sec: "#0A64BC",
+        ter: "#0057ff",
+        for: "#1769ff",
+      },
+      fontFamily: {
+        "Nrj-fonts": ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
